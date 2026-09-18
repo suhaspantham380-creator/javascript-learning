0c916bdd76146bdd6f4555623b2a9c7bf2b8edcd
Sameer <121498481+sam7eer@users.noreply.github.com>
2026-09-18 16:17:40 +0530

    adding of two Numbers

---
+++ function_day1/addNumbers.js
@@ -0,0 +1,10 @@
+function addNumbers (num1 , num2){
+    return num1+num2 ;
+}
+
+// 4 , 9 
+
+// The sum of the numbers is : 13 
+let result = addNumbers(4,9);
+
+console.log("The sum of number is " + result);
