e365804d868e1e34fe4a461fd4de87deeea2f645
Sameer <121498481+sam7eer@users.noreply.github.com>
2026-09-18 16:33:29 +0530

    multipleparameters

--- function_day1/addNumbers.js
+++ function_day1/addNumbers.js
@@ -1,5 +1,5 @@
 function addNumbers (num1 , num2){
-    return num1+num2 ;
+     return num1+num2 ;
 }
 
 // 4 , 9 
---
+++ function_day1/mutipleparameters.js
@@ -0,0 +1,15 @@
+function addNumbers (num1 , num2){
+     return num1 + num2;
+}
+
+function multiplyNumbers(num3 , num4){
+    return num3 * num4;
+}
+
+
+let result1 = addNumbers(2, 3); 
+
+let result2 = multiplyNumbers(2, 3);
+ 
+console.log(result1);
+console.log(result2); 
